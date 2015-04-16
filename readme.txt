9f1e9cd85f550c46c53ca76d73e116426f4f2184
toledoroy <toledoroy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-16 08:57:32 +0000

    New Tag, V2.0.0
    git-svn-id: https://plugins.svn.wordpress.org/virtual-brick-social-bar-side-widget/tags/2.0.0@1136003 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,63 @@
+=== VB Social Share Widget ===
+Contributors: toledoroy
+Tags: facebook, twitter, google +1, pinterest, social, share, like
+Requires at least: 3.0
+Tested up to: 4.1
+Stable tag: 2.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Easy To Use Social Bar with Your Favorite Social Share Buttons 
+
+* Facebook Like + Share
+* Twitter
+* Google +1 
+* Pinterest
+
+
+== Installation ==
+
+Activate the plugin, go to widgets page and add the widget to the desired location on the Sidebar.
+
+* If Facebook Like button doesn't show check the Init Facebook checkbox 
+
+
+== Frequently Asked Questions ==
+
+= How to Configure my SocialBar ? =
+Go to Admin -> Widgets, Select the Social Bar Widget and Expand 
+Here you can select the title you want and enable or disable buttons
+
+= Why is Facebook Button not Showing ? =
+For facebook buttons to work you must have a facebook Init which your site might already have if you installed other facebook extensions
+If not, mark the Facebook Init checkbox to solve this issue.
+Note that this should only be done once, in case of multple widgets in page make sure you only activate the Init function on the first one.
+
+
+== Screenshots ==
+1. Front End Screenshot
+2. Front End Screenshot on a different template
+
+== Changelog ==
+
+= 2.0.0 =
+* Added Optional Facebook Init Function
+
+= 1.2.2 =
+* Updated Info
+
+= 1.2.1 =
+* Admin Design Changes Only
+
+= 1.2.0 =
+* Button Selection - Select Only The Buttons You Want  
+
+= 1.1.0 =
+* Layout Changes
+
+= 1.0.0 =
+* First Stable Version
+
+== Upgrade Notice ==
---
+++ widget.php
@@ -0,0 +1,89 @@
+<?php
+/*
+Plugin Name: Virtual Brick Social Bar Side Widget
+Plugin URI: http://www.virtual-brick.com/?ref=socialBar
+Description: a Social Bar with Your favorite Social Share Buttons 
+Version: 2.0.0
+Author: Roy Toledo
+Author URI: http://www.virtual-brick.com/?ref=socialBar
+License: GPL
+*/
+
+add_action( 'widgets_init', 'vb_social_bar_widget_register_widget' );
+function vb_social_bar_widget_register_widget() { register_widget( 'SocialBarWidget' ); }
+
+
+class SocialBarWidget extends WP_Widget 
+{
+
+	public function __construct() 
+	{
+		parent::__construct(
+			'vb-social_bar_widget',
+			__( 'Social Bar Widget' ),
+			array( 'description' => __( 'Social Bar Sidebar Widget' ) )
+		);
+	}
+
+	public function form( $instance ) 
+	{
+		?>
+		<style type="text/css">
+			.vb-social_bar_field input[type="checkbox"] { float: left; margin: 0 10px 0 0;}
+		</style>
+		<?php
+		//Widget Form Fields
+		$form_options['widget_fields']['title'] = array('label'=>'Title:', 'type'=>'text', 'default'=>__( 'Share This!' ));
+		$form_options['widget_fields']['show-facebook'] = array('label'	=>'Facebook Like', 'type'=>'checkbox', 'default'=>true);
+		$form_options['widget_fields']['show-google'] = array('label'	=>'Google +1', 'type'=>'checkbox', 'default'=>true);
+		$form_options['widget_fields']['show-twitter'] = array('label'	=>'Twitter', 'type'=>'checkbox', 'default'=>true);
+		$form_options['widget_fields']['show-pinterest'] = array('label'=>'Pinterest', 'type'=>'checkbox', 'default'=>false);
+		$form_options['widget_fields']['facebook-init'] = array('label'	=>'Facebook Init (Check if Facebook button not showing)', 'type'=>'checkbox', 'default'=>false);
+		
+		//Load Defaults
+		if (!$instance){
+			foreach($form_options['widget_fields'] as $key => $field) {
+				$instance[$key] = esc_attr($field['default']);
+			}
+		}
+		
+		//Print Fields
+		foreach($form_options['widget_fields'] as $key => $field) {
+			$field_name = sprintf('%s_%s_%s', $form_options['prefix'], $key, $number);
+			$field_checked = '';
+			if ($field['type'] == 'text') {
+				$field_value = htmlspecialchars($instance[$key], ENT_QUOTES);
+			}elseif ($field['type'] == 'checkbox'){
+				$field_value = 1;
+				if (! empty($instance[$key])) {
+					$field_checked = 'checked="checked"';
+				}
+			}
+			?>
+			<p class="vb-social_bar_field">
+				<label for="<?php echo $this->get_field_id( $key ); ?>"><?php _e( $field['label'] ); ?></label> 
+				<input class="" id="<?php echo $this->get_field_id( $key ); ?>" name="<?php echo $this->get_field_name( $key ); ?>" type="<?php echo $field['type']?>" value="<?php echo $field_value ?>" <?php echo $field_checked?>/>
+			</p>
+			<?
+		}
+	}
+
+	public function update( $new_instance, $old_instance ) 
+	{
+		$instance= $new_instance;
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		return $instance;
+	}
+
+	public function widget( $args, $instance ) 
+	{
+		echo $args['before_widget'];
+		if ( ! empty( $instance['title'] ) ) {
+			echo $args['before_title'];
+			echo esc_html( $instance['title'] );
+			echo $args['after_title'];
+		}
+		include 'bar.inc';
+		echo $args['after_widget'];
+	}
+}
